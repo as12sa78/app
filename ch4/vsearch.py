35c674647d872d7d79fb3bf5a7d944005641617f
Vit Mar <as12sa78@gmail.com>
2024-06-27 13:13:30 +0300

    добавил анатацию в 1 строку.

--- ch4/vsearch.py
+++ ch4/vsearch.py
@@ -1,4 +1,4 @@
-def search4vowels(word):
+def search4vowels(word:str): -> set:
     """Выводит гласные, найденные во введенном слове"""
     vowers = set('аеиоуёяюэ')
     return vowers.intersection(set(word))
