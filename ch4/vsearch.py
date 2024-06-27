5beda2957344af676634d016f621ade1aa4bf58f
Vit Mar <as12sa78@gmail.com>
2024-06-27 17:02:17 +0300

    меняю заголовок.

--- ch4/vsearch.py
+++ ch4/vsearch.py
@@ -1,5 +1,5 @@
 def search4vowels(word:str) -> set:
-    """Выводит гласные, найденные во введенном слове"""
+    """Выводит гласные, найденные во введенном слове."""
     vowers = set('аеиоуёяюэ')
     return vowers.intersection(set(word))
     
