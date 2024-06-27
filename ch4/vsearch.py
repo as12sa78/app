97f40c277fb00495eac8aa3e552fdcbf2d4a8980
Vit Mar <as12sa78@gmail.com>
2024-06-27 17:14:32 +0300

    add english vowels

--- ch4/vsearch.py
+++ ch4/vsearch.py
@@ -1,5 +1,5 @@
 def search4vowels(word:str) -> set:
     """Выводит гласные, найденные во введенном слове."""
-    vowers = set('аеиоуёяюэ')
+    vowers = set('аеиоуёяюэaeiouy')
     return vowers.intersection(set(word))
     
