112c9979bc0cdf8ee5e5a21cba89542841e41340
Vit Mar <as12sa78@gmail.com>
2024-06-28 16:07:07 +0300

    function провкрки semantic

---
+++ ch4/mystery.py
@@ -0,0 +1,9 @@
+def double(arg):
+    print('Вначале: ', arg)
+    arg = arg * 2
+    print('После: ', arg)
+
+def change(arg):
+    print('Вначале: ', arg)
+    arg.append('Доп. данные')
+    print('После: ', arg)
