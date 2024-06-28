35afa5eed48ff3cb7c9ef79d073f5825fcc878c8
Vit Mar <as12sa78@gmail.com>
2024-06-28 08:54:22 +0300

    add funkcion search4letters

--- ch4/vsearch.py
+++ ch4/vsearch.py
@@ -2,4 +2,13 @@ def search4vowels(word:str) -> set:
     """Выводит гласные, найденные во введенном слове."""
     vowers = set('аеиоуёяюэaeiouy')
     return vowers.intersection(set(word))
+
+def search4letters(phrase:str, letters:str) ->set:
+    """Возвращает множество букв из 'letters', найденных
+      в указанной фразе."""
+    # set(letters) - создаёт обьект множества из letters.
+
+    # .intersection - находит пересечение множества созданного
+    # из letters с множеством созданным из phrase.
+    return set(letters).intersection(set(phrase))
     
