a067a5b239f0ed9280f3204328c746d59429175e
Vit Mar <as12sa78@gmail.com>
2024-06-28 10:27:12 +0300

    create setup dir

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name='vsearch',
+    version='1.0',
+    description='Поисковая утилита',
+    author='Пишу я с книги',
+    author_email='roga_kopyta@gmail.com',
+    url='roga_i_kopyta.com',
+    py_modules=['vsearch'],
+)
