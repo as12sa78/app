686451d0442d104d6a35af031583200217947a86
Vit Mar <as12sa78@gmail.com>
2024-07-03 10:42:41 +0300

    add vsearch.log in project

--- ch5/webapp/wsearch4web.py
+++ ch5/webapp/wsearch4web.py
@@ -3,6 +3,11 @@ from vsearch import search4letters #Импорт написанного мною
 
 app = Flask(__name__)
 
+#  Функция принимает два аргумента и записывает их в файл.
+def log_reqest(req: 'flask_reqest', res: str) -> None:
+  # with управляет контекстом вызова файла vsearch.log
+  with open('vsearch.log', 'a') as log: # vsearch.log открывается как наполняемый и переменная log указывает на дискриптор этого файла.
+    print(req, res, file=log) # Записывает значения переменных в файл.
 
 # Добавить функцию представления для декоратора route 
 # с маршрутом('/search4')
@@ -12,6 +17,7 @@ def do_search() -> str:
   letters = request.form['letters']
   title = 'Результаты поиска.'
   results = str(search4letters(phrase, letters))
+  log_reqest(request.form, results)
 
 # Возвращаем сгенерированый HTML-шаблон 'result.html' с переданными переменными.
   return render_template('result.html',
@@ -20,10 +26,13 @@ def do_search() -> str:
                          the_letters=letters,
                          the_results=results,)
   
+  
 @app.route('/') 
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html', the_title='Привет!')
+
+
         
 if __name__ == '__main__':
    app.run()
